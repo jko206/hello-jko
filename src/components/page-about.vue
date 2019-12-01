5005ab9dc511825a41debe13d38af1911ca9ebee
J.Ko <just.jai.ko@gmail.com>
2019-11-30 16:14:44 -0800

    make about page

--- src/components/page-about.vue
+++ src/components/page-about.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <h1>About me</h1>
-    <img src="../assets/family.jpg" alt="Ko family" class="main-photo" />
-    <p>
-      My name is Jai Ko. It might be tempting to pronounce it as Jī, rhyming with "hi",
-      but it's actually Jā, rhyming with "hey". The reason is fairly simple: my parents
-      are immigrants who aren't proficient in English. 
-    </p>
+  <div class="page-about">
+    <img 
+      src="../assets/family.jpg" 
+      alt="Ko family" 
+      class="main-photo" 
+      title="At a pumpkin patch in Concord, CA, in 2019"
+    />
+    <div class="paragraphs">
+      <p>
+        My name is Jai Ko. It might be tempting to pronounce it as Jī, 
+        rhyming with "hi", but it's actually Jā, rhyming with "hey". 
+        The reason is fairly simple: my parents are immigrants 
+        who aren't proficient in English. 
+      </p>
+      <p>
+        My hobbies include programming, solving puzzles, and playing Vainglory. 
+        My topics of interest are politics, technology, science, business, 
+        economy, monetary instruments, comedy, comics and cartoons.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -17,9 +29,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.page-about {
+  display: grid;
+  grid-column-gap: 30px;
+  grid-template-columns: 400px auto;
+}
+
 .main-photo {
-  float: left;
-  width: 300px;
-  margin: 10px;
+  width: 100%;
+}
+p {
+  max-width: 600px;
+  line-height: 2;
+  padding: 10px;
+  margin-top: 0;
 }
 </style>
