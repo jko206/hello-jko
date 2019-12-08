ca6080b4b38e7b4359c3c60f234707009b87eaed
J.Ko <just.jai.ko@gmail.com>
2019-12-08 11:50:42 -0800

    create tooltip component

---
+++ src/components/app-tooltip.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="tooltip-wrapper">
+    <div class="tooltip-trigger" 
+      :style="{display: triggerDisplayStyle}"
+      @click="showTooltip"
+    >
+      <slot name="tooltip-trigger" />
+    </div>
+    <div :class="['tooltip', {visible: isTooltipVisibile}]">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTooltip',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    triggerDisplayStyle: {
+      type: String, 
+      required: false,
+      default: 'inline-block',
+      validator: display => ['inline', 'block', 'inline-block'].includes(display)
+    },
+  },
+  data() {
+    return {
+      isTooltipVisibile: false, 
+      timeout: null,
+    }
+  },
+  methods: {
+    showTooltip() {
+      this.isTooltipVisibile = true
+      // eslint-disable-next-line
+      console.log('here')
+      this.timeout = setTimeout(() => {
+        this.isTooltipVisibile = false
+      }, 3000)
+    },
+    hideTooltip() {
+      this.isTooltipVisibile = false
+      if(this.timeout) clearTimeout(this.timeout)
+      this.timeout = null
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tooltip-trigger:hover + .tooltip {
+  visibility: visible;
+}
+.tooltip {
+  background: #333;
+  color: white;
+  padding: 0.3em;
+  text-align: center;
+  position: absolute;
+  right: -85px;
+  width: 200px;
+  top: -4em;
+  border-radius: 3px;
+  visibility: hidden;
+  font-size: 0.9em;
+  line-height: 1.5;
+
+  &.visible {
+    visibility: visible;
+  }
+
+  &::after {
+    content: '';
+    border-width: 5px;
+    border-style: solid;
+    border-color: transparent transparent #333 #333;   
+    transform: rotate(-45deg);
+    position: absolute;
+    top: 85%;
+    left: 50%;
+  }
+}
+</style>
