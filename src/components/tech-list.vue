22d1b0e49fc6b9e06e31724dd7faf2855b7f60cd
J.Ko <just.jai.ko@gmail.com>
2019-12-08 15:47:51 -0800

    create tech list

---
+++ src/components/tech-list.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tech-list">
+    <span 
+      v-for="item in techList" 
+      :key="item"
+      class="tech-stack-item"
+    >
+      {{ item }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TechList',
+  props: {
+    techList: {
+      type: Array,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tech-list span {
+  display: block;
+  text-align: right;
+}
+</style>
