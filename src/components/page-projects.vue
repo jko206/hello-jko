236ba6f437b845ca5b111df9771f91c2b54d4de1
J.Ko <just.jai.ko@gmail.com>
2019-12-09 12:21:30 -0800

    add content

--- src/components/page-projects.vue
+++ src/components/page-projects.vue
@@ -18,30 +18,129 @@
     <div class="content-block full-height">
       <div class="title-wrapper">
         <h2>KenKen Solver</h2>
+        <div class="icon-links">
           <icon-link 
             link="https://codesandbox.io/s/elated-hoover-2bnn0"
             icon="codesandbox"
           />
+          <icon-link 
+            icon="github"
+            :underConstruction="true"
+          />
         </div>
-        <tech-list :techList="['HTML', 'CSS', 'JavaScript']" />
+        <tech-list :techList="['HTML', 'CSS', 'JavaScript', 'jQuery']" />
       </div>
-      <div class="content double-col">
+      <div class="content faux-double-col">
         <p>
-          Virtually all websites utilize the neatly stacking nature of 90&deg; angles. 
-          I explored what angles would look nice on the web and discovered that hexagons
-          stack nicely. This is a small example to illustrate how hexagons might look on the web.
+          <a href="https://kenkenpuzzle.com" target="_blank">KenKen</a> is a puzzle that is 
+          somewhat similar to sudoku, but it requires arithmetic to solve it. 
         </p>
         <p>
-          This example uses grid to layout the hexagons. High school level of trigonometry
-          is used to calculate the grid-row-gap and grid-column-gap. 
+          I made an app that can solve the puzzle. 
         </p>
       </div>
     </div>
+    
+    <div class="content-block full-height">
+      <div class="title-wrapper">
+        <h2>Knight's Tour</h2>
+        <div class="icon-links">
+          <icon-link 
+            link="https://codesandbox.io/s/loving-bash-s5u4q"
+            icon="codesandbox"
+          />
+          <icon-link 
+            icon="github"
+            :underConstruction="true"
+          />
+        </div>
+        <tech-list :techList="['HTML', 'CSS', 'JavaScript', 'jQuery']" />
+      </div>
+      <div class="content faux-double-col">
+        <p>
+          Knight's tour is a program that can shows the path a knight can
+          take from a given position to visit all the squares. 
+        </p>
+      </div>
+    </div>
+    
+    <div class="content-block full-height">
+      <div class="title-wrapper">
+        <h2>Toptal Screening Project</h2>
+        <div class="icon-links">
+          <icon-link 
+            link="https://88uws.csb.app/"
+            icon="codesandbox"
+          />
+          <icon-link 
+            link="https://github.com/jaiko86/toptal"
+            icon="github"
+          />
+        </div>
+        <tech-list :techList="['HTML', 'CSS', 'JavaScript', 'Vue', 'SASS', 'Responsive Design']" />
+      </div>
+      <div class="content faux-double-col">
+        <p>
+          Toptal is one of the world's leading network of freelance developers.
+          Entry into the network requires passing screening tests and projects. 
+          This is the project that let me pass the screening.
+        </p>
+      </div>
+    </div>
+    
+    <div class="content-block full-height">
+      <div class="title-wrapper">
+        <h2>Tweet Streams</h2>
+        <div class="icon-links">
+          <icon-link 
+            link="https://tweet-streams.firebaseapp.com/"
+            icon="demo"
+          />
+          <icon-link 
+            link="https://github.com/jaiko86/tweet-streams"
+            icon="github"
+          />
+        </div>
+        <tech-list :techList="[
+          'HTML5', 
+          'CSS', 
+          'JavaScript', 
+          'Vue', 
+          'SASS', 
+          'Responsive Design', 
+          'Firebase',
+          'REST API',
+        ]"/>
+      </div>
+      <div class="content double-col">
+        <p>
+          This was a screening project for turing.ly. 
+        </p>
+        <p>
+          In this project, I used Twitter's API to fetch
+          real-time tweets for three accounts. 
+        </p>
+        <p>
+          Some of the notable features:
+        </p>
+        <ul>
+          <li>
+            Drag and drop to rearrange columns, either in 
+            the main view or in the menu. The feature works
+            on mobile as well
+          </li>
+          <li>
+            Hover over date to see the timestamp
+          </li>
+          <li>
+            Uses <code>localStorage</code> to save user preference
+          </li>
+        </ul>
+      </div>
+    </div>
+    
     <!-- 
       project list:
-        - Knight's tour
-        - Toptal projects
-        - Twitter
         - Precision.js
      -->
   </div>
--- src/components/page-work.vue
+++ src/components/page-work.vue
@@ -10,8 +10,67 @@
           I am monetizing or will eventually monetize it.
         </p>
         <p>
-          Any available screenshots, repos, or demos will have a link below
-          the header for each project. 
+          Any available screenshots or demos will have a link below
+          the header for each project. For obvious reasons, I can't or
+          won't post link to the repositories. 
+        </p>
+      </div>
+    </div>
+    
+    <div class="content-block full-height">
+      <div class="title-wrapper">
+        <h2>MathTestNinja</h2>
+        <div class="icon-links">
+          <icon-link 
+            link="https://mathtest.ninja/"
+            icon="demo"
+          />
+        </div>
+        <tech-list :techList="[
+          'HTML', 
+          'CSS', 
+          'JavaScript', 
+          'Vue',
+          'Vue-router',
+          'Vuex',
+          'Vue-test-util',
+          'Jest',
+          'SASS', 
+          'Responsive Design',
+          'Firebase'
+        ]" />
+      </div>
+      <div class="content double-col">
+        <p>
+          MathTestNinja is a service I started as a side project to help
+          students prepare for standardized math tests, such as ACT, SAT, and GED.
+        </p>
+        <p>
+          I was a private tutor from about 2010 to 2017. I've taught a wide
+          range of students on a couple of different topics, but mostly math
+          of all levels from elementary schools to seniors in high school. 
+          During that time I realized there was a lot of room for improvements
+          in all areas of education.
+        </p>
+        <p>
+          That was that.
+        </p>
+        <p>
+          But at some point, I began to learn that social inequalities in the US
+          happen because underrepresented minorities have a smaller chance of getting
+          degree-requiring, high paying jobs, such as software engineering jobs. 
+        </p>
+        <p>
+          While the problem is bigger than that, I reasoned that it'd do everyone
+          good for these test prep courses' prices to come down. That was the first
+          motivation. The second motivation is that test prep isn't complicated, and
+          can easily be automated. My 7 years of private tutoring in math tells me
+          that you can raise your math score from mid 400s to low 600s in a matter 
+          of 3 months by simply practicing them a bunch of times. 
+        </p>
+        <p>
+          So that's how this started. This is something I spend about 10 hours a week on,
+          and something I hope to run as a passive income generator in years to come.
         </p>
       </div>
     </div>
@@ -26,11 +85,19 @@
 </template>
 
 <script>
-export default {
+import IconLink from './icon-link.vue'
+import TechList from './tech-list.vue'
 
+export default {
+  name: 'PageWork', 
+  components: {
+    IconLink,
+    TechList,
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../style/global';
 
 </style>
